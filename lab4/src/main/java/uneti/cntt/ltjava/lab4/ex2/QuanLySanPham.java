b44e35cbb2a19305e94ca5283cef344f457db39c
phaml <phaml@DESKTOP-K719701>
2021-08-21 11:14:36 +0700

    them bai moi

---
+++ lab4/src/main/java/uneti/cntt/ltjava/lab4/ex2/QuanLySanPham.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uneti.cntt.ltjava.lab4.ex2;
+
+/**
+ *
+ * @author phaml
+ */
+public class QuanLySanPham {
+    static void menu(){
+        System.out.println("1. ");
+    }
+    public static void main(String[] args) {
+        
+    }
+}
